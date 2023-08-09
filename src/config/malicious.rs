4826db7a32fbe7986047428f7b8b5af132c5815b
SetarehGhorshi <ssghorsh@uwaterloo.ca>
2023-08-09 11:36:42 +0000

    getting inputs fixed

--- src/config/malicious.rs
+++ src/config/malicious.rs
@@ -1,7 +1,7 @@
 // error handling
 use crate::TofndResult;
 use anyhow::anyhow;
-
+use tracing::debug;
 use tofn::{collections::TypedUsize, gg20::keygen::malicious::Behaviour as KeygenBehaviour};
 
 use clap::App;
@@ -34,25 +34,25 @@ pub fn get_behaviour_matches(app: App) -> TofndResult<Behaviours> {
         .value_of("victim")
         .ok_or_else(|| anyhow!("victim value"))?;
     
-    let victim: Result<Vec<usize>, _> = victim_str
+    let victimp: Result<Vec<usize>, _> = victim_str
         .split(',')
         .map(|s| s.parse::<usize>())
         .collect();
     
-    let victim: Vec<usize> = victim.unwrap();
+    victim = victimp.unwrap();
     let faulty_str = matches
     .value_of("faulty")
     .ok_or_else(|| anyhow!("faulty value"))?;
 
-let faulty: Result<Vec<usize>, _> = faulty_str
+let faultyp: Result<Vec<usize>, _> = faulty_str
     .split(',')
     .map(|s| s.parse::<usize>())
     .collect();
 
-let faulty: Vec<usize> = faulty.unwrap();
+faulty = faultyp.unwrap();
 
     }
-
+    debug!("{:?} {:?} ",victim,faulty);
     // TODO: parse keygen malicious types as well
     //  let keygen = KeygenBehaviour::R1BadCommit;
     let keygen = match_string_to_behaviour(behaviour, victim, faulty);
@@ -68,6 +68,7 @@ fn match_string_to_behaviour(
     // let victim = TypedUsize::from_usize(victim);
     let victim= _victim.iter().map(|&x| TypedUsize::from_usize(x)).collect();
     let faulty= _faulty.iter().map(|&x| TypedUsize::from_usize(x)).collect();
+    
     // let faulty = TypedUsize::from_usize(faulty);
     // TODO: some of the behaviours do not demand a victim. In the future, more
     // will be added that potentially need different set of arguments.
